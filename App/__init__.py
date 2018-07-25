a2062c57bc4d37d084d4ffd228b995f71561aa7e
laoxu <xsmlxh666@163.com>
2018-07-25 19:02:55 +0800

    在App包中的入口文件 __init__.py 中创建Flask程序，并进行初始化     - 初始化Flask自己     - 使用配置文件，在Flask上进行设置     - 初始化蓝图，也就是初始化Views     - 初始化第三方，初始化ext

---
+++ App/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from App import settings
+from App.ext import init_ext
+from App.views import init_first_blue
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    # 配置加载
+    app.config.from_object(settings.envs.get("develop"))
+
+    # 初始化各种第三方库
+    init_ext(app)
+
+    # 初始化蓝图
+    init_first_blue(app)
+
+    return app
