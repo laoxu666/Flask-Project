8b3f0ea4782a8d3de250e8fca927e57751202a68
laoxu <xsmlxh666@163.com>
2018-07-25 21:07:29 +0800

    -数据库 增 删 该 操作

--- App/views.py
+++ App/views.py
@@ -19,13 +19,58 @@ def index():
     return "Hello Flask Project！"
 
 
+# 添加数据
 @blue.route("/addstudent/")
 def add_student():
 
     student = Student()
-    student.s_name = "小明%d" %random.randrange(100)
-
+    student.s_name = "小明%d" % random.randrange(100)
+    # 添加数据操作
     db.session.add(student)
     db.session.commit()
 
     return "Student Create Success!"
+
+
+# 添加一组数据
+@blue.route("/addstudents/")
+def add_students():
+
+    students = []
+
+    for i in range(10):
+
+        student = Student()
+        student.s_name = "小王%d" % random.randrange(100)
+
+        students.append(student)
+
+    # 添加一组学生
+    db.session.add_all(students)
+    db.session.commit()
+
+    return "一组学生创建成功！"
+
+
+# 先查询后删除
+@blue.route("/deletestudent/")
+def delete_student():
+
+    student = Student.query.first()
+    # 删除数据操作
+    db.session.delete(student)
+    db.session.commit()
+
+    return "Student Delete Success!"
+
+
+# 先查询后修改
+@blue.route("/modifystudent/")
+def modify_student():
+    student = Student.query.first()
+    student.s_name = "小花3"
+
+    db.session.add(student)
+    db.session.commit()
+
+    return "修改学生成功！"
